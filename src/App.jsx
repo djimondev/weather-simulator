99d24e0aef520814abcc56994156c5ac8663af34
Jimmy Lefevre <jlefevre@vaultinum.com>
2024-07-01 13:24:24 +0200

    refactor(App.jsx, devices.js): ♻️ move initialDevices data from App.jsx to a new file devices.js for better code organization

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,9 @@ import { Stack } from "@mui/material";
 import { useEffect, useState } from "react";
 import "./App.css";
 import DeviceCard from "./DeviceCard";
+import { devices as initialDevices } from "./services/devices";
 
 function App() {
-    const initialDevices = [
-        {
-            id: 1,
-            name: "Device 1",
-            status: "On",
-            temperature: 25,
-            humidity: 40
-        },
-        {
-            id: 2,
-            name: "Device 2",
-            status: "Off",
-            temperature: 30,
-            humidity: 50
-        },
-        {
-            id: 3,
-            name: "Device 3",
-            status: "On",
-            temperature: 27,
-            humidity: 45
-        },
-        {
-            id: 4,
-            name: "Device 4",
-            status: "Off",
-            temperature: 28,
-            humidity: 60
-        },
-        {
-            id: 5,
-            name: "Device 5",
-            status: "On",
-            temperature: 26,
-            humidity: 55
-        }
-    ];
-
     const [devices, setDevices] = useState(initialDevices);
 
     useEffect(() => {
---
+++ src/services/devices.js
@@ -0,0 +1,37 @@
+export const devices = [
+    {
+        id: 1,
+        name: "Device 1",
+        status: "On",
+        temperature: 25,
+        humidity: 40
+    },
+    {
+        id: 2,
+        name: "Device 2",
+        status: "Off",
+        temperature: 30,
+        humidity: 50
+    },
+    {
+        id: 3,
+        name: "Device 3",
+        status: "On",
+        temperature: 27,
+        humidity: 45
+    },
+    {
+        id: 4,
+        name: "Device 4",
+        status: "Off",
+        temperature: 28,
+        humidity: 60
+    },
+    {
+        id: 5,
+        name: "Device 5",
+        status: "On",
+        temperature: 26,
+        humidity: 55
+    }
+];
