a3d9ee1c8bafd88d01282d43757bee4085a31571
Jimmy Lefevre <jlefevre@vaultinum.com>
2024-07-01 15:25:40 +0200

    feat(App.jsx, DeviceCard.jsx): ✨ add device creation and deletion functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { CircularProgress, Stack } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import { Alert, Button, Card, CardContent, CircularProgress, Stack, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import "./App.css";
 import { DeviceCard } from "./DeviceCard";
-import { getDevice, getDevices } from "./services/devices";
+import { createDevice, getDevice, getDevices } from "./services/devices";
 
 function App() {
     const [devices, setDevices] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isAddingDevice, setIsAddingDevice] = useState(false);
+    const [newDeviceName, setNewDeviceName] = useState("");
 
-    const reloadDevices = async () => {
+    const reloadDevices = async (displayIsLoading = false) => {
+        setIsLoading(displayIsLoading);
         getDevices().then(devices => {
             setDevices(devices);
+            setIsLoading(false);
         });
     };
 
@@ -26,11 +32,62 @@ function App() {
     }, []);
 
     return (
-        <Stack spacing={4} direction="row" flexWrap="wrap" useFlexGap justifyContent={"center"}>
-            {devices.length === 0 && <CircularProgress />}
-            {devices.map(device => (
-                <DeviceCard key={device.id} device={device} reloadDevices={reloadDevices} reloadDevice={reloadDevice} />
-            ))}
+        <Stack direction={"column"} spacing={3}>
+            {!isLoading && !!devices.length && (
+                <Button size="small" startIcon={<AddIcon />} onClick={() => setIsAddingDevice(true)}>
+                    New device
+                </Button>
+            )}
+            <Stack spacing={4} direction="row" flexWrap="wrap" useFlexGap justifyContent={"center"}>
+                {!!isLoading && <CircularProgress />}
+                {!isLoading && !devices.length && (
+                    <Alert
+                        severity="info"
+                        action={
+                            <Button size="small" startIcon={<AddIcon />} onClick={() => setIsAddingDevice(true)}>
+                                New device
+                            </Button>
+                        }
+                    >
+                        The is no device here
+                    </Alert>
+                )}
+
+                {isAddingDevice && (
+                    <Card>
+                        <CardContent>
+                            <Stack spacing={2} direction="row" width={"100%"} justifyContent={"center"} alignItems={"center"}>
+                                <TextField
+                                    label="Device Name"
+                                    variant="outlined"
+                                    size="small"
+                                    onChange={e => setNewDeviceName(e.target.value)}
+                                    value={newDeviceName}
+                                />
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    size="small"
+                                    onClick={() => {
+                                        setIsLoading(true);
+                                        setIsAddingDevice(false);
+                                        createDevice({ name: newDeviceName, status: "Off", temperature: 0, humidity: 0 }).then(() => {
+                                            reloadDevices(true);
+                                            setNewDeviceName("");
+                                            setIsLoading(false);
+                                        });
+                                    }}
+                                >
+                                    Add
+                                </Button>
+                            </Stack>
+                        </CardContent>
+                    </Card>
+                )}
+                {!isLoading &&
+                    !!devices.length &&
+                    devices.map(device => <DeviceCard key={device.id} device={device} reloadDevices={reloadDevices} reloadDevice={reloadDevice} />)}
+            </Stack>
         </Stack>
     );
 }
--- src/DeviceCard.jsx
+++ src/DeviceCard.jsx
@@ -1,80 +1,144 @@
-import { Card, CardActions, CardContent, CardHeader, CircularProgress, Slider, Stack, ToggleButton, Typography } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import {
+    Box,
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    CircularProgress,
+    IconButton,
+    Menu,
+    MenuItem,
+    Slider,
+    Stack,
+    ToggleButton,
+    Typography
+} from "@mui/material";
 import { useState } from "react";
-import { patchDevice } from "./services/devices";
+import { deleteDevice, patchDevice } from "./services/devices";
 
 export const DeviceCard = ({ device, reloadDevices, reloadDevice }) => {
     const [isLoading, setIsLoading] = useState(false);
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = event => {
+        console.log(event.currentTarget);
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleDelete = () => {
+        confirm("Are you sure you want to delete this device?") && performDelete();
+    };
+
+    const performDelete = () => {
+        patchDevice(device.id, { status: "Off" }).then(() => {
+            setTimeout(() => {
+                deleteDevice(device.id).then(() => {
+                    reloadDevices(true);
+                });
+            }, 500);
+        });
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
     return (
-        <Card style={{ minWidth: "15vw", padding: "0 30px" }}>
-            <CardHeader title={device.name} action={isLoading && <CircularProgress size={20} thickness={5} />}></CardHeader>
-            <CardContent>
-                <Stack py={2} spacing={1} direction="column" width={"100%"} justifyContent={"center"} alignItems={"center"}>
-                    <Stack spacing={2} direction="row" width={"100%"} justifyContent={"center"} alignItems={"center"}>
-                        <Typography variant="body1">Status:</Typography>
-                        <Typography variant="h6">{device.status}</Typography>
+        <>
+            <Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+                <MenuItem onClick={handleClose}>Edit</MenuItem>
+                <MenuItem onClick={handleDelete}>Delete</MenuItem>
+            </Menu>
+            <Card style={{ minWidth: "15vw", padding: "0 30px" }}>
+                <CardHeader
+                    title={
+                        <Stack spacing={2} direction="row" width={"100%"} justifyContent={"space-between"} alignItems={"start"}>
+                            <Stack direction={"column"} justifyContent={"start"} alignItems={"start"}>
+                                <Typography variant="h6">Device {device.name}</Typography>
+                                <Typography variant="body2">{device.id}</Typography>
+                            </Stack>
+                            <Box width={20} height={6}>
+                                {isLoading ? (
+                                    <CircularProgress size={30} />
+                                ) : (
+                                    <IconButton aria-label="settings" onClick={e => handleClick(e)}>
+                                        <MoreVertIcon />
+                                    </IconButton>
+                                )}
+                            </Box>
+                        </Stack>
+                    }
+                ></CardHeader>
+
+                <CardContent>
+                    <Stack py={2} spacing={1} direction="column" width={"100%"} justifyContent={"center"} alignItems={"center"}>
+                        <Stack spacing={2} direction="row" width={"100%"} justifyContent={"center"} alignItems={"center"}>
+                            <Typography variant="body1">Status:</Typography>
+                            <Typography variant="h6">{device.status}</Typography>
+                        </Stack>
+                        <Stack spacing={2} direction="row" width={"100%"} justifyContent={"center"} alignItems={"center"}>
+                            <Typography variant="body1">Temperature:</Typography>
+                            <Typography variant="h6">{device.temperature}°C</Typography>
+                        </Stack>
+                        <Stack spacing={2} direction="row" width={"100%"} justifyContent={"center"} alignItems={"center"}>
+                            <Typography variant="body1">Humidity:</Typography>
+                            <Typography variant="h6">{device.humidity}%</Typography>
+                        </Stack>
                     </Stack>
-                    <Stack spacing={2} direction="row" width={"100%"} justifyContent={"center"} alignItems={"center"}>
-                        <Typography variant="body1">Temperature:</Typography>
-                        <Typography variant="h6">{device.temperature}°C</Typography>
-                    </Stack>
-                    <Stack spacing={2} direction="row" width={"100%"} justifyContent={"center"} alignItems={"center"}>
-                        <Typography variant="body1">Humidity:</Typography>
-                        <Typography variant="h6">{device.humidity}%</Typography>
-                    </Stack>
-                </Stack>
-            </CardContent>
-            <CardActions>
-                <Stack spacing={2} direction="column" width={"100%"} mx={2}>
-                    <ToggleButton
-                        value="check"
-                        selected={device.status === "On"}
-                        onClick={() => {
-                            setIsLoading(true);
-                            patchDevice(device.id, { status: device.status === "On" ? "Off" : "On" }).then(() => {
-                                reloadDevices();
-                                setIsLoading(false);
-                            });
-                        }}
-                    >
-                        {device.status === "On" ? "On" : "Off"}
-                    </ToggleButton>
-                    <Slider
-                        defaultValue={device.temperature}
-                        disabled={device.status === "Off"}
-                        step={10}
-                        marks
-                        min={-20}
-                        max={70}
-                        onChange={e => {
-                            setIsLoading(true);
-                            patchDevice(device.id, { temperature: e.target.value }).then(() => {
-                                setTimeout(() => {
+                </CardContent>
+                <CardActions>
+                    <Stack spacing={2} direction="column" width={"100%"} mx={2}>
+                        <ToggleButton
+                            value="check"
+                            selected={device.status === "On"}
+                            onClick={() => {
+                                setIsLoading(true);
+                                patchDevice(device.id, { status: device.status === "On" ? "Off" : "On" }).then(() => {
                                     reloadDevices();
                                     setIsLoading(false);
-                                }, 500);
-                            });
-                        }}
-                    />
-                    <Slider
-                        defaultValue={device.humidity}
-                        disabled={device.status === "Off"}
-                        step={10}
-                        marks
-                        min={0}
-                        max={100}
-                        onChange={e => {
-                            setIsLoading(true);
-                            patchDevice(device.id, { humidity: e.target.value }).then(() => {
-                                setTimeout(() => {
-                                    reloadDevices();
-                                    setIsLoading(false);
-                                }, 500);
-                            });
-                        }}
-                    />
-                </Stack>
-            </CardActions>
-        </Card>
+                                });
+                            }}
+                        >
+                            {device.status === "On" ? "On" : "Off"}
+                        </ToggleButton>
+                        <Slider
+                            defaultValue={device.temperature}
+                            disabled={device.status === "Off"}
+                            step={10}
+                            marks
+                            min={-20}
+                            max={70}
+                            onChange={e => {
+                                setIsLoading(true);
+                                patchDevice(device.id, { temperature: e.target.value }).then(() => {
+                                    setTimeout(() => {
+                                        reloadDevices();
+                                        setIsLoading(false);
+                                    }, 500);
+                                });
+                            }}
+                        />
+                        <Slider
+                            defaultValue={device.humidity}
+                            disabled={device.status === "Off"}
+                            step={10}
+                            marks
+                            min={0}
+                            max={100}
+                            onChange={e => {
+                                setIsLoading(true);
+                                patchDevice(device.id, { humidity: e.target.value }).then(() => {
+                                    setTimeout(() => {
+                                        reloadDevices();
+                                        setIsLoading(false);
+                                    }, 500);
+                                });
+                            }}
+                        />
+                    </Stack>
+                </CardActions>
+            </Card>
+        </>
     );
 };
